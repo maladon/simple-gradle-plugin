082c659ef95104a7b91829dd37a03cc2ff0ab673
maladon <phil@maladon.com>
2013-03-28 14:29:33 -0500

    PHT - adding gradle build

--- README.md
+++
@@ -1,4 +0,0 @@
-simple-gradle-plugin
-====================
-
-Simple gradle plugin to demonstrate how to debug and step through gradle plugins
---
+++ src/main/groovy/com/objectpartners/simplegradleplugin/SimpleGradlePlugin.groovy
@@ -0,0 +1,11 @@
+package com.objectpartners.simplegradleplugin
+
+import org.apache.tools.ant.Project
+import org.gradle.api.Plugin
+
+class SimpleGradlePlugin  implements Plugin<Project> {
+    @Override
+    void apply(Project t) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
